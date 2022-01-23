b4a6dde2d8fef697687ccfa8bc152e803da10ea3
vu2rmk <vu2rmk@gmail.com>
2022-01-24 00:52:07 +0530

    added all the models

---
+++ src/main/java/com/vu2rmk/fleetapp/controllers/ApplicationController.java
@@ -0,0 +1,13 @@
+package com.vu2rmk.fleetapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+
+    @GetMapping("/index")
+    public String goHome(){
+        return "index";
+    }
+}
