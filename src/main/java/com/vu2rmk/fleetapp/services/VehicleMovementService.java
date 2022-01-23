eb79029abb21f1bda3d8e12503db14fd639a433d
vu2rmk <vu2rmk@gmail.com>
2022-01-24 01:19:45 +0530

    created base service

---
+++ src/main/java/com/vu2rmk/fleetapp/services/VehicleMovementService.java
@@ -0,0 +1,8 @@
+
+package com.vu2rmk.fleetapp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleMovementService {
+}
---
+++ src/main/java/com/vu2rmk/fleetapp/services/VehicleService.java
@@ -0,0 +1,7 @@
+package com.vu2rmk.fleetapp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleService {
+}
