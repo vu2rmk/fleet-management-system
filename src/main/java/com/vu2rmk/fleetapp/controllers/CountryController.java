bdea4d1a99853747a03527cae72a8144c4ea7fd3
vu2rmk <vu2rmk@gmail.com>
2022-01-24 17:31:49 +0530

    listing and adding of countries

--- src/main/java/com/vu2rmk/fleetapp/controllers/CountryController.java
+++ src/main/java/com/vu2rmk/fleetapp/controllers/CountryController.java
@@ -1,13 +1,32 @@
 package com.vu2rmk.fleetapp.controllers;
 
+import com.vu2rmk.fleetapp.models.Country;
+import com.vu2rmk.fleetapp.services.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class CountryController {
 
+    @Autowired
+    private CountryService countryService;
+
     @GetMapping("/countries")
-    public String getCountries(){
+    public String getCountries(Model model){
+
+        List<Country> countryList = countryService.getCountries();
+        model.addAttribute("countries",countryList);
         return "country";
+    }
+
+    @PostMapping("/countries/addNew")
+    public String addNew(Country country) {
+        countryService.save(country);
+        return "redirect:/countries";
     }
 }
--- src/main/java/com/vu2rmk/fleetapp/services/CountryService.java
+++ src/main/java/com/vu2rmk/fleetapp/services/CountryService.java
@@ -1,7 +1,23 @@
 package com.vu2rmk.fleetapp.services;
 
+import com.vu2rmk.fleetapp.models.Country;
+import com.vu2rmk.fleetapp.repositories.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public List<Country> getCountries(){
+        return countryRepository.findAll();
+    }
+
+    public void save(Country country){
+        countryRepository.save(country);
+    }
 }
