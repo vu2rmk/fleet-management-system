cd5b41b67a17944fac0d84600969452e6b15631f
vu2rmk <vu2rmk@gmail.com>
2022-01-27 23:56:56 +0530

    added client functionality

--- src/main/java/com/vu2rmk/fleetapp/controllers/ClientController.java
+++ src/main/java/com/vu2rmk/fleetapp/controllers/ClientController.java
@@ -1,12 +1,65 @@
 package com.vu2rmk.fleetapp.controllers;
 
+import com.vu2rmk.fleetapp.models.Country;
+import com.vu2rmk.fleetapp.models.Client;
+import com.vu2rmk.fleetapp.models.State;
+import com.vu2rmk.fleetapp.services.CountryService;
+import com.vu2rmk.fleetapp.services.ClientService;
+import com.vu2rmk.fleetapp.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @Autowired
+    private CountryService countryService;
+
+    @Autowired
+    private StateService stateService;
+
     @GetMapping("/clients")
-    public String getClients(){
-        return "Client";
+    public String getClients(Model model){
+
+        List<Client> clientList = clientService.getClients();
+        model.addAttribute("clients",clientList);
+
+        List<Country> countryList = countryService.getCountries();
+        model.addAttribute("countries",countryList);
+
+        List<State> stateList = stateService.getStates();
+        model.addAttribute("states",stateList);
+        return "client";
+    }
+
+    @PostMapping("/clients/addNew")
+    public String addNew(Client client) {
+        clientService.save(client);
+        return "redirect:/clients";
+    }
+
+    @RequestMapping("clients/findById")
+    @ResponseBody
+    public Client findById(Integer id){
+        return clientService.findById(id);
+    }
+
+    @RequestMapping(value = "/clients/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Client client) {
+        clientService.save(client);
+        return "redirect:/clients";
+    }
+
+    @RequestMapping(value = "/clients/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        clientService.delete(id);
+        return "redirect:/clients";
     }
 }
--- src/main/java/com/vu2rmk/fleetapp/services/ClientService.java
+++ src/main/java/com/vu2rmk/fleetapp/services/ClientService.java
@@ -1,7 +1,31 @@
 package com.vu2rmk.fleetapp.services;
 
+import com.vu2rmk.fleetapp.models.Client;
+import com.vu2rmk.fleetapp.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public List<Client> getClients(){
+        return clientRepository.findAll();
+    }
+
+    public void save(Client client){
+        clientRepository.save(client);
+    }
+
+    public Client findById(Integer id){
+        return clientRepository.findById(id).get();
+    }
+
+    public void delete(Integer id) {
+        clientRepository.deleteById(id);
+    }
 }
