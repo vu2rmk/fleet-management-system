4e497ada01fdf6854ed97b84627ff6987dd471d6
vu2rmk <vu2rmk@gmail.com>
2022-01-28 00:50:24 +0530

    added invoice functionality

--- src/main/java/com/vu2rmk/fleetapp/controllers/InvoiceController.java
+++ src/main/java/com/vu2rmk/fleetapp/controllers/InvoiceController.java
@@ -1,13 +1,67 @@
 package com.vu2rmk.fleetapp.controllers;
 
+import com.vu2rmk.fleetapp.models.Client;
+import com.vu2rmk.fleetapp.models.Country;
+import com.vu2rmk.fleetapp.models.Invoice;
+import com.vu2rmk.fleetapp.models.InvoiceStatus;
+import com.vu2rmk.fleetapp.services.ClientService;
+import com.vu2rmk.fleetapp.services.CountryService;
+import com.vu2rmk.fleetapp.services.InvoiceService;
+import com.vu2rmk.fleetapp.services.InvoiceStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class InvoiceController {
 
+    @Autowired
+    private InvoiceStatusService invoiceStatusService;
+
+    @Autowired
+    private InvoiceService invoiceService;
+
+    @Autowired
+    private ClientService clientService;
+
     @GetMapping("/invoices")
-    public String getInvoices(){
-        return "Invoice";
+    public String getCountries(Model model){
+
+        List<Invoice> invoiceList = invoiceService.getInvoices();
+        model.addAttribute("invoices",invoiceList);
+
+        List<Client> clientList = clientService.getClients();
+        model.addAttribute("clients",clientList);
+
+        List<InvoiceStatus> invoiceStatusList = invoiceStatusService.getInvoiceStatuses();
+        model.addAttribute("invoiceStatuses",invoiceStatusList);
+        return "invoice";
+    }
+
+    @PostMapping("/invoices/addNew")
+    public String addNew(Invoice invoice) {
+        invoiceService.save(invoice);
+        return "redirect:/invoices";
+    }
+
+    @RequestMapping("invoices/findById")
+    @ResponseBody
+    public Invoice findById(Integer id){
+        return invoiceService.findById(id);
+    }
+
+    @RequestMapping(value = "/invoices/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Invoice invoice) {
+        invoiceService.save(invoice);
+        return "redirect:/invoices";
+    }
+
+    @RequestMapping(value = "/invoices/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        invoiceService.delete(id);
+        return "redirect:/invoices";
     }
 }
--- src/main/java/com/vu2rmk/fleetapp/services/InvoiceService.java
+++ src/main/java/com/vu2rmk/fleetapp/services/InvoiceService.java
@@ -1,7 +1,30 @@
 package com.vu2rmk.fleetapp.services;
 
+import com.vu2rmk.fleetapp.models.Invoice;
+import com.vu2rmk.fleetapp.repositories.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class InvoiceService {
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+    public List<Invoice> getInvoices(){
+        return invoiceRepository.findAll();
+    }
+
+    public void save(Invoice invoice){
+        invoiceRepository.save(invoice);
+    }
+
+    public Invoice findById(Integer id){
+        return invoiceRepository.findById(id).get();
+    }
+
+    public void delete(Integer id) {
+        invoiceRepository.deleteById(id);
+    }
 }
