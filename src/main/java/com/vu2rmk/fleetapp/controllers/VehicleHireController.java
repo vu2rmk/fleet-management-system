e529b268bf607afce65307526a1677f1d3e10e81
vu2rmk <vu2rmk@gmail.com>
2022-02-16 15:48:24 +0530

    added vehicle hire functionality

--- src/main/java/com/vu2rmk/fleetapp/controllers/VehicleHireController.java
+++ src/main/java/com/vu2rmk/fleetapp/controllers/VehicleHireController.java
@@ -1,13 +1,64 @@
 package com.vu2rmk.fleetapp.controllers;
 
+import com.vu2rmk.fleetapp.models.VehicleHire;
+import com.vu2rmk.fleetapp.services.ClientService;
+import com.vu2rmk.fleetapp.services.LocationService;
+import com.vu2rmk.fleetapp.services.VehicleHireService;
+import com.vu2rmk.fleetapp.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class VehicleHireController {
 
-    @GetMapping("/vehiclehires")
-    public String getVehicleHires(){
-        return "VehicleHire";
+    @Autowired
+    private VehicleHireService vehicleHireService;
+
+    @Autowired
+    private ClientService clientService;
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @Autowired
+    private LocationService locationService;
+
+    @GetMapping("/vehicleHires")
+    public String getVehicleHires(Model model){
+
+        List<VehicleHire> vehicleHireList = vehicleHireService.getVehicleHires();
+        model.addAttribute("vehicleHires", vehicleHireList);
+        model.addAttribute("vehicles", vehicleService.getVehicles());
+        model.addAttribute("locations", locationService.getLocations());
+        model.addAttribute("clients", clientService.getClients());
+        return "vehicleHire";
+    }
+
+    @PostMapping("/vehicleHires/addNew")
+    public String addNew(VehicleHire vehicleHire) {
+        vehicleHireService.save(vehicleHire);
+        return "redirect:/vehicleHires";
+    }
+
+    @RequestMapping("vehicleHires/findById")
+    @ResponseBody
+    public VehicleHire findById(Integer id){
+        return vehicleHireService.findById(id);
+    }
+
+    @RequestMapping(value = "/vehicleHires/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(VehicleHire vehicleHire) {
+        vehicleHireService.save(vehicleHire);
+        return "redirect:/vehicleHires";
+    }
+
+    @RequestMapping(value = "/vehicleHires/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        vehicleHireService.delete(id);
+        return "redirect:/vehicleHires";
     }
 }
--- src/main/java/com/vu2rmk/fleetapp/services/VehicleHireService.java
+++ src/main/java/com/vu2rmk/fleetapp/services/VehicleHireService.java
@@ -1,8 +1,32 @@
 
 package com.vu2rmk.fleetapp.services;
 
+import com.vu2rmk.fleetapp.models.VehicleHire;
+import com.vu2rmk.fleetapp.repositories.VehicleHireRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VehicleHireService {
+
+    @Autowired
+    private VehicleHireRepository vehicleHireRepository;
+
+    public List<VehicleHire> getVehicleHires(){
+        return vehicleHireRepository.findAll();
+    }
+
+    public void save(VehicleHire vehicleHire){
+        vehicleHireRepository.save(vehicleHire);
+    }
+
+    public VehicleHire findById(Integer id){
+        return vehicleHireRepository.findById(id).get();
+    }
+
+    public void delete(Integer id) {
+        vehicleHireRepository.deleteById(id);
+    }
 }
